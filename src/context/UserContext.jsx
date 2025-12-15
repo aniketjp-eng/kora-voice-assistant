47fba2f91c5c414d04bf809cb72004a81b97a346
Aniket Prajapati <aniketjp.eng@gmail.com>
2025-12-15 21:03:26 +0530

    add log response features

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext } from "react";
+import { runChat } from "../gemini";
 export const datacontext = createContext();
 
 function UserContext({children}) {
@@ -10,13 +11,23 @@ function UserContext({children}) {
         text_speak.lang = "hi-GB";
         window.speechSynthesis.speak(text_speak);
     }
+
+    async function aiResponse(prompt) {
+        let text = await runChat(prompt);
+        speak(text);
+    };
+
     let speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
 
     let recognition = new speechRecognition();
     recognition.onresult = ((e) => {
         let transciptResult = (e.results[0])[0].transcript;
           console.log(transciptResult);
-    })
+          aiResponse(transciptResult);
+    }
+
+)
+
     let value = {
         recognition
     };
